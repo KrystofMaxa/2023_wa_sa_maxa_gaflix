f58ca573ae312e13deb73cc41e13c18e5f6bdda1
KrystofMaxa <94890860+KrystofMaxa@users.noreply.github.com>
2024-05-31 08:01:07 +0200

    Update admin.py

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,29 +1,36 @@
 from django.contrib import admin
 from filmy.models import Movie, Director, Genre, Actor, Fotbal
-
 class MovieAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id","name","year","footage","director"]
+    list_display_links = ["id","name"]
+    search_fields = ["id","name", "director__name"]
+    list_filter = ["year","genres"]
+    list_editable = ["year","footage", "director"]
+
+    list_display = ["id", "name", "year", "footage", "director", "genres_display"]
+    list_display_links = ["id", "name"]
+    search_fields = ["=id", "name", "director__name"]
+    list_filter = ["year", "genres"]
+    list_editable = ["year", "footage"]
 
 
 class DirectorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "name", "birth_year"]
+    list_display_links = ["id", "name"]
+    search_fields = ["=id", "name"]
+    list_filter = ["birth_year"]
+    list_editable = ["birth_year"]
 
 
 class GenreAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["id", "name"]
+    list_display_links = ["id", "name"]
+    search_fields = ["=id", "name"]
+
 
 class ActorAdmin(admin.ModelAdmin):
-    pass
-
-class FotbalAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Movie, MovieAdmin)
-
-admin.site.register(Director, DirectorAdmin)
-
-admin.site.register(Genre, GenreAdmin)
-
-admin.site.register(Actor, ActorAdmin)
-
-admin.site.register(Fotbal, FotbalAdmin)
+    list_display = ["id", "name", "birth_year"]
+    list_display_links = ["id", "name"]
+    search_fields = ["=id", "name"]
+    list_filter = ["birth_year"]
+    list_editable = ["birth_year"]
