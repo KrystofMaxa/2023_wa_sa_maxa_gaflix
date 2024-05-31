4c44400f5da3ff8705eae508a669be2ccf470729
KrystofMaxa <94890860+KrystofMaxa@users.noreply.github.com>
2024-05-31 08:03:04 +0200

    Update models.py

--- filmy/models.py
+++ filmy/models.py
@@ -8,20 +8,42 @@ class Movie(models.Model):
         blank=True, null=True, help_text="in minutes"
     )
     description = models.TextField(blank=True)
+
+    main_picture = models.CharField(blank=True, default="", max_length=2000)
+    director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
+    actors = models.ManyToManyField('Actor', blank=True, null=True)
+    genres = models.ManyToManyField('Genre', blank=True, null=True)
+
     main_picture = models.ImageField(blank=True, null=True)
+    director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
+    actors = models.ManyToManyField('Actor', blank=True)
+    genres = models.ManyToManyField('Genre', blank=True)
+
 
     def __str__(self):
         return self.name
+
+    def genres_display(self):
+        return ",".join([i.name for i in self.genres.all()])
 
 
 class Director(models.Model):
     name = models.CharField(max_length=300, null=True)
     birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
     description = models.TextField(blank=True)
-    main_picture = models.ImageField(blank=True, null=True)
+    main_picture = models.CharField(blank=True, default="", max_length=2000)
+
 
     def __str__(self):
         return self.name
+
+    def genres_display(self):
+
+        # a = self.genres.all().order_by("name")
+        # out = ""
+        # for i in a:
+        #     out += f"{i.name}, "
+        return ", ".join([i.name for i in self.all()])
 
 
 class Genre(models.Model):
